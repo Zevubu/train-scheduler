c7bfa2a956c623e9b75d07e7ab72c10bf816e531
Zev Hoffman <zevubu@gmail.com>
2019-08-05 00:11:24 -0700

    wrote out begining plan in js

--- assets/javascript/trainScript.js
+++ assets/javascript/trainScript.js
@@ -1,1 +1,20 @@
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+let averageTrainInterval = 7;
+let firstTrain = "03:00"
+let lastTrain = "23:00"
+
+// Start with 1 train and 1 location and 1 direction.
+
+// Future plans:
+
+// inBound and outBound
+// Determine number of stops and how they are spaced
+// Average speed, also including speed up and slow down. 
+// So just Average time between each stop. 
+// maybe I could add in obsticles that hold the train up? 
+
+
+// original time pushed back a year so it's definetly before this moment. 
+
+// current time. 
