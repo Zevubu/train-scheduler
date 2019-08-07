fde865b1f1144fdffd7c8b4a696fa3d30709b621
Zev Hoffman <zevubu@gmail.com>
2019-08-06 23:38:38 -0700

    got my form working.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # train-scheduler
 A page that uses momentjs to determine display when trains will be arriving and departing.  
 
+before you start create a folder inside of the assets/javascript file called keys.js.
+Inside of keys.js print the fallowing variable.
+    let FBKEY = "Put your fire base key here" 
+
+crazy idea 2 gangs two trains 
+
+Each trying to get there supplies to various stops along the way. 
+
+How to set this up two opponents who have a train. Each train has several stops each stop has an average time between stops. Your goal is to get you train to each destination the quickest. You use your arsenal to enforce your train and deplete there's 
+
+
 Requirements
 App must suit these basic specs:
 When adding trains, administrators should be able to submit the following:
--- assets/javascript/trainScript.js
+++ assets/javascript/trainScript.js
@@ -1,21 +1,80 @@
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+window.onload = function(){
+    let newName;
+    let trainDestination;
+    let firstTime;
+    let frequency;
+    
+    // link to firebase.
+    var firebaseConfig = {
+        apiKey: FBKEY,
+        authDomain: "train-schedule-ccc8f.firebaseapp.com",
+        databaseURL: "https://train-schedule-ccc8f.firebaseio.com",
+        projectId: "train-schedule-ccc8f",
+        storageBucket: "https://train-schedule-ccc8f.firebaseio.com/",
+        messagingSenderId: "828168838602",
+        appId: "1:828168838602:web:a1d62e2124039ada"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-let averageTrainInterval = 7;
-let firstTrain = "03:00"
-let lastTrain = "23:00"
-// link to firebase.
+    let database = firebase.database();
+    console.log(database);
 
-// Start with 1 train and 1 location and 1 direction.
+    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-// Future plans:
-// use time converter to keep time up to date. 
-// inBound and outBound
-// Determine number of stops and how they are spaced
-// Average speed, also including speed up and slow down. 
-// So just Average time between each stop. 
-// maybe I could add in obsticles that hold the train up? 
+    let averageTrainInterval = 7;
+    let firstTrain = "03:00"
+    let lastTrain = "23:00"
 
 
-// original time pushed back a year so it's definetly before this moment. 
+    let newTrainBtn = document.getElementById("new-train");
+    let nameInput = document.getElementById("name-input");
+    let destinationInput = document.getElementById("destination-input");
+    let timeInput = document.getElementById("first-time-input");
+    let frequencyInput = document.getElementById("frequency-input");
+    
 
-// current time. 
+    newTrainBtn.addEventListener("click", function(){
+        event.preventDefault();
+        newName = nameInput.value.trim();
+        console.log(newName);
+        trainDestination = destinationInput.value.trim();
+        console.log(trainDestination);
+        firstTime = timeInput.value.trim();
+        console.log(firstTime);
+        frequency = frequencyInput.value.trim();
+        console.log(frequency);
+
+
+
+    })
+
+
+
+    // pull data from my trian form.
+
+    // push that data to fire base
+    // pull from firebase to produce train data.
+    // use timers to keep train running and looping 
+    
+
+    // Future plans:
+    // use time converter to keep time up to date. 
+    // When train arives hide main area and display train img with sound.
+    // inBound and outBound
+    // Determine number of stops and how they are spaced.
+    // Average speed, also including speed up and slow down. 
+    // So just Average time between each stop. 
+    // maybe I could add in obsticles that hold the train up?
+    // compeate with another player to keep there train preocupied. 
+    // 
+    
+    // stops are set.
+    // You are given a group of mersanaries who go and try and take on the train.
+    // 
+
+
+    // original time pushed back a year so it's definetly before this moment. 
+
+    // current time. 
+};
